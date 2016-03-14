a0fd143fd7811ec349b54650539f60043cf46027
AMargheriti <eliasdjurfeldt@gmail.com>
2016-03-14 20:52:41 +0000

    Removed the "FloatingActionButton" from every page

--- app/src/main/java/mansci/gradesapp/manSciModules/be1.java
+++ app/src/main/java/mansci/gradesapp/manSciModules/be1.java
@@ -20,14 +20,5 @@ public class be1 extends AppCompatActivity {
         setContentView(R.layout.modules_activity_be1);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
