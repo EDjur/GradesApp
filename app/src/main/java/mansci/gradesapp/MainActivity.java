03c2e0410b444fcd2f6fa62b47bae9b1931ca1d1
AMargheriti <eliasdjurfeldt@gmail.com>
2016-03-18 23:00:20 +0000

    Added a dummy sort textfield at bottom of main screen that alphabetically sorts the 16 mansci modules. We want to add this in the fragment.

--- app/src/main/java/mansci/gradesapp/MainActivity.java
+++ app/src/main/java/mansci/gradesapp/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import mansci.gradesapp.calculations.Calculations;
 
 public class MainActivity extends Activity {
+    String[] modules = new String[16];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +43,10 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void chooseYear(View view){
+    public void chooseYear(View view) {
         Button button = (Button) view;
 
-        switch (button.getId()){
+        switch (button.getId()) {
             case R.id.main_button_2ndyear:
                 Intent intent = new Intent(this, ManSciSecondYear.class);
                 startActivity(intent);
@@ -58,10 +59,54 @@ public class MainActivity extends Activity {
 
         }
     }
-    public void bestModule(View view){
-        TextView textView = (TextView) view;
-        Calculations calculations = new Calculations(this);
 
-        textView.setText(calculations.summary());
+    public void bestModule(View view) {
+        TextView textView = (TextView) view;
+
+        modules[0] = getResources().getString(R.string.manSci_firstYear_MFOM1);
+        modules[1] = getResources().getString(R.string.manSci_firstYear_MFOM2);
+        modules[2] = getResources().getString(R.string.manSci_firstYear_cat);
+        modules[3] = getResources().getString(R.string.manSci_firstYear_be1);
+        modules[4] = getResources().getString(R.string.manSci_firstYear_asm);
+        modules[5] = getResources().getString(R.string.manSci_firstYear_bs);
+        modules[6] = getResources().getString(R.string.manSci_firstYear_da1);
+        modules[7] = getResources().getString(R.string.manSci_firstYear_be2);
+
+        modules[8] = getResources().getString(R.string.manSci_secondYear_MFOM3);
+        modules[9] = getResources().getString(R.string.manSci_secondYear_strategy);
+        modules[10] = getResources().getString(R.string.manSci_secondYear_CT);
+        modules[11] = getResources().getString(R.string.manSci_secondYear_finance);
+        modules[12] = getResources().getString(R.string.manSci_secondYear_design);
+        modules[13] = getResources().getString(R.string.manSci_secondYear_PTOM);
+        modules[14] = getResources().getString(R.string.title_activity_da2);
+        modules[15] = getResources().getString(R.string.manSci_secondYear_minor);
+
+        // SORT ALPHA LOL xD
+        String text = "";
+        for (int i = 0; i < modules.length - 1; i++) {
+            text += "\n" + modules[i];
+        }
+        textView.setText(text);
+        sortModules(modules);
+        String text2 = "";
+        for (int i = 0; i < modules.length - 1; i++) {
+            text2 += "\n" + modules[i];
+            System.out.print(modules[i]);
+        }
+        textView.setText(text2);
+    }
+
+    public void sortModules(String[] modules) {
+        String t;
+        for (int i = 0; i < modules.length; i++) {
+            for (int j = 0; j < modules.length - 1 - i; j++) {
+                if (modules[j].compareTo(modules[j + 1]) > 0) {
+                    t = modules[j];
+                    modules[j] = modules[j + 1];
+                    modules[j + 1] = t;
+                }
+            }
+        }
     }
 }
+
