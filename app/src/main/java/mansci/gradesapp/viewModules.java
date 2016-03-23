7e0f82e9af7f5ae02c075517d36e8738dac0b982
AMargheriti <eliasdjurfeldt@gmail.com>
2016-03-23 10:23:42 +0000

    Added Scrollview for first year. Added alphaSort of modules. Added landscape mode for first year.

--- app/src/main/java/mansci/gradesapp/viewModules.java
+++ app/src/main/java/mansci/gradesapp/viewModules.java
@@ -1,68 +1,27 @@
 package mansci.gradesapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import mansci.gradesapp.calculations.Calculations;
+import org.w3c.dom.Text;
 
-public class MainActivity extends Activity {
+public class viewModules extends AppCompatActivity {
     String[] modules = new String[16];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_view_modules);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void chooseYear(View view) {
-        Button button = (Button) view;
-
-        switch (button.getId()) {
-            case R.id.main_button_2ndyear:
-                Intent intent = new Intent(this, ManSciSecondYear.class);
-                startActivity(intent);
-                break;
-            case R.id.main_button_1styear:
-                Intent intent2 = new Intent(this, ManSciFirstYear.class);
-                startActivity(intent2);
-                break;
-
-
-        }
-    }
-
-    public void viewModules(View view){
-        Intent intent = new Intent(this, viewModules.class);
-        startActivity(intent);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        bestModule(findViewById(R.id.modules_textview));
     }
 
     public void bestModule(View view) {
@@ -86,23 +45,17 @@ public class MainActivity extends Activity {
         modules[14] = getResources().getString(R.string.title_activity_da2);
         modules[15] = getResources().getString(R.string.manSci_secondYear_minor);
 
-        // SORT ALPHA LOL xD
         String text = "";
         for (int i = 0; i < modules.length - 1; i++) {
             text += "\n" + modules[i];
         }
         textView.setText(text);
-        sortModules(modules);
-        String text2 = "";
-        for (int i = 0; i < modules.length - 1; i++) {
-            text2 += "\n" + modules[i];
-            System.out.print(modules[i]);
-        }
-        textView.setText(text2);
+
     }
 
-    public void sortModules(String[] modules) {
+    public void alphaSort(View view) {
         String t;
+        TextView textView = (TextView) findViewById(R.id.modules_textview);
         for (int i = 0; i < modules.length; i++) {
             for (int j = 0; j < modules.length - 1 - i; j++) {
                 if (modules[j].compareTo(modules[j + 1]) > 0) {
@@ -112,6 +65,12 @@ public class MainActivity extends Activity {
                 }
             }
         }
+        String text2 = "";
+        for (int i = 0; i < modules.length - 1; i++) {
+            text2 += "\n" + modules[i];
+            System.out.print(modules[i]);
+        }
+        textView.setText(text2);
     }
-}
 
+}
